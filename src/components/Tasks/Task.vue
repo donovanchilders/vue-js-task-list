1e31213484476fdf794ef781a0e09b1a73a13553
Donovan Childers <tsound@hotmail.com>
2019-12-29 13:28:21 -0800

    Style tasks Remove unused styles

--- src/components/Tasks/Task.vue
+++ src/components/Tasks/Task.vue
@@ -1,8 +1,8 @@
 <template>
-    <li class="list-group-item pl-0">
-        <label>
+    <li class="list-group-item p-0">
+        <label :class="{ 'completed': task.completed }" :title="task.completed ? 'Mark incomplete' : 'Mark complete'">
             <input type="checkbox" v-model="task.completed" @change="toggleTask" class="mr-2" />
-            <span :class="{ 'completed': task.completed }">{{ task.name }}</span>
+            <span>{{ task.name }}</span>
             <button type="button" class="close ml-2" aria-label="Close" @click.prevent="deleteTask(task.id)" v-if="task.completed" title="Delete task">
                 <span aria-hidden="true">&times;</span>
             </button>
@@ -31,24 +31,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    span.completed {
-        text-decoration: line-through;
-    }
-    span.delete-task-action {
-        background-color: #eee;
-        border: solid 1px #ccc;
-        border-radius: 50%;
+    label {
         cursor: pointer;
-        display: inline-block;
-        font-weight: bold;
-        margin: 0 .5rem; 
-        width: 1rem;
-        height: 1rem;
-        line-height: 1rem;
-        text-align: center;
-        vertical-align: middle;
-        &:active, &:hover {
-            background-color: #ddd;
+        display: block;
+        margin: 0;
+        padding: 0.5rem;
+        &:hover {
+            background-color: #eee;
+        }
+        &.completed {
+            background-color: #efefef;
+            text-decoration: line-through;
         }
     }
 </style>
